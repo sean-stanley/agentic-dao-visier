050b0d2b453e0ac70a8f544df0df78abbc2f9358
ChrisKw0n <ckwo873@aucklanduni.ac.nz>
2025-02-04 22:50:54 +1300

    feat: initialised erc20 conctract

---
+++ stylus-dao/dao-token/src/erc20.rs
@@ -0,0 +1,35 @@
+// Imported packages
+use alloc::string::String;
+use alloy_primitives::{Address, U256};
+use alloy_sol_types::sol;
+use core::marker::PhantomData;
+use stylus_sdk::{
+    evm,
+    msg,
+    prelude::*,
+};
+
+pub trait Erc20Params {
+    /// Immutable token name
+    const NAME: &'static str;
+
+    /// Immutable token symbol
+    const SYMBOL: &'static str;
+
+    /// Immutable token decimals
+    const DECIMALS: u8;
+}
+
+sol_storage! {
+    /// Erc20 implements all ERC-20 methods.
+    pub struct Erc20<T> {
+        /// Maps users to balances
+        mapping(address => uint256) balances;
+        /// Maps users to a mapping of each spender's allowance
+        mapping(address => mapping(address => uint256)) allowances;
+        /// The total supply of the token
+        uint256 total_supply;
+        /// Used to allow [`Erc20Params`]
+        PhantomData<T> phantom;
+    }
+}
