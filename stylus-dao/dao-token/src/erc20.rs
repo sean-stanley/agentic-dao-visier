ba08e4f4fa0465d9e0e0af443f4370af8b86f670
ChrisKw0n <ckwo873@aucklanduni.ac.nz>
2025-02-04 22:57:57 +1300

    feat: implement transfer and mint function

--- stylus-dao/dao-token/src/erc20.rs
+++ stylus-dao/dao-token/src/erc20.rs
@@ -18,6 +18,9 @@ pub trait Erc20Params {
 
     /// Immutable token decimals
     const DECIMALS: u8;
+
+    /// Immutable token total supply
+    const TOTAL_SUPPLY: U256;
 }
 
 sol_storage! {
@@ -25,11 +28,83 @@ sol_storage! {
     pub struct Erc20<T> {
         /// Maps users to balances
         mapping(address => uint256) balances;
-        /// Maps users to a mapping of each spender's allowance
+
+        /// Maps users to allowances
         mapping(address => mapping(address => uint256)) allowances;
-        /// The total supply of the token
-        uint256 total_supply;
+
+        /// Total supply of the token
+        U256 total_supply;
+
         /// Used to allow [`Erc20Params`]
         PhantomData<T> phantom;
+    }
+}
+
+// Declare events and Solidity error types
+sol! {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    error InsufficientBalance(address from, uint256 have, uint256 want);
+    error InsufficientAllowance(address owner, address spender, uint256 have, uint256 want);
+}
+
+/// Represents the ways methods may fail.
+#[derive(SolidityError)]
+pub enum Erc20Error {
+    InsufficientBalance(InsufficientBalance),
+    InsufficientAllowance(InsufficientAllowance),
+}
+
+// Methods marked as "pub" here are usable outside of the erc20 module (i.e. they're callable from lib.rs)
+impl<T: Erc20Params> Erc20<T> {
+    /// Movement of funds between 2 accounts
+    /// (invoked by the external transfer() and transfer_from() functions )
+    pub fn _transfer(
+        &mut self,
+        from: Address,
+        to: Address,
+        value: U256,
+    ) -> Result<(), Erc20Error> {
+        // Decreasing sender balance
+        let mut sender_balance = self.balances.setter(from);
+        let old_sender_balance = sender_balance.get();
+        if old_sender_balance < value {
+            return Err(Erc20Error::InsufficientBalance(InsufficientBalance {
+                from,
+                have: old_sender_balance,
+                want: value,
+            }));
+        }
+        sender_balance.set(old_sender_balance - value);
+
+        // Increasing receiver balance
+        let mut to_balance = self.balances.setter(to);
+        let new_to_balance = to_balance.get() + value;
+        to_balance.set(new_to_balance);
+
+        // Emitting the transfer event
+        evm::log(Transfer { from, to, value });
+        Ok(())
+    }
+
+    /// Mints `value` tokens to `address`
+    pub fn mint(&mut self, address: Address, value: U256) -> Result<(), Erc20Error> {
+        // Increasing balance
+        let mut balance = self.balances.setter(address);
+        let new_balance = balance.get() + value;
+        balance.set(new_balance);
+
+        // Increasing total supply
+        self.total_supply.set(self.total_supply.get() + value);
+
+        // Emitting the transfer event
+        evm::log(Transfer {
+            from: Address::ZERO,
+            to: address,
+            value,
+        });
+
+        Ok(())
     }
 }
